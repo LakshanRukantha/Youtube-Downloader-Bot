662595595896202184327100cf9c663b339acdc6
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2021-11-01 11:52:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,6 +4,8 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
+    joinButton = InlineKeyboardMarkup([
+        [])
     welcomed = f"Hello <b>{message.from_user.first_name} ☺️</b>\n\nUseful Commands\n\n💠 /help - How to use this bot\n\n💠 /ping - Check bot status\n\n💠 /report - Report any questions to admin"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
