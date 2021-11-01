e63fe2232e2c5c98c5f70103ec3657f5214d935b
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2021-11-01 11:14:01 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,13 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Follow these three simple steps.\n
-
-1) Send URL of YouTube video do you want to download. 😃\n
-
-2) Select quality of video or audio do you want to download. 🤩\n
-
-3) Download ✅\n
-
-Contact @Lakshan_Rukantha for report bugs and problems of this bot."
+    helptxt = f"Follow these three simple steps.\n1) Send URL of YouTube video do you want to download. 😃\n2) Select quality of video or audio do you want to download. 🤩\n3) Download ✅\nContact @Lakshan_Rukantha for report bugs and problems of this bot."
     await message.reply_text(helptxt)
